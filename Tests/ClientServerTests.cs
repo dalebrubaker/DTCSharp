eb4375a28b494f096fea54ada91b0bd5821acb57
Dale Brubaker <Dale@BruSoftware.com>
2016-10-29 15:35:19 -0400

    Started Tests project for unit and integration tests

---
+++ Tests/ClientServerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestServer;
+using Xunit;
+
+namespace Tests
+{
+    public class ClientServerTests : IDisposable
+    {
+        public ClientServerTests()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing");
+        }
+
+        [Fact]
+        public void LogonTest()
+        {
+            var serverStub = new ServerStub();
+        }
+    }
+}
