940132b092bd61725ed719991b67e221eab2b4b1
DaleBrubaker <brubaker.dale@gmail.com>
2022-04-18 12:47:18 -0400

    Read Serilog config

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 
 namespace TestServer
@@ -15,20 +16,26 @@ namespace TestServer
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-
+            
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
             var seqURL = Environment.GetEnvironmentVariable("SeqURL");
             var apiKey = Environment.GetEnvironmentVariable("DTCSharpSeqApiKey");
-            if (seqURL != null)
+            if (string.IsNullOrEmpty(seqURL))
             {
                 Log.Logger = new LoggerConfiguration()
-                    .MinimumLevel.Verbose()
-                    .Enrich.FromLogContext()
-                    .Enrich.WithProcessId()
-                    .Enrich.WithThreadId()
-                    .Enrich.WithThreadName()
-                    .Enrich
-                    .WithProperty("Application", nameof(TestServer))
-                    .WriteTo.Seq(seqURL, apiKey: apiKey)
+                    .ReadFrom
+                    .Configuration(configuration)
+                    .CreateLogger();
+            }
+            else
+            {
+                // Add Seq using environment variables, to keep them out of appsettings.json
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.Seq(seqURL, apiKey:apiKey)
+                    .ReadFrom
+                    .Configuration(configuration)
                     .CreateLogger();
             }
             try
